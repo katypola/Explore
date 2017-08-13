725aff58a8dcb89568586f61b680ea17a52327f1
Katy Pola <katyp03@gmail.com>
2017-08-13 16:51:28 -0400

    Formatted navbar

--- app/assets/javascripts/fields.js
+++ app/assets/javascripts/fields.js
@@ -1,5 +1,42 @@
-// start of tilt scroll:
+// start of astronomy field page:
+document.addEventListener("turbolinks:load", function(){
 
+(function($) {
+    "use strict";
+  
+    var $navbar = $(".nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    //scroll top 0 sticky
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 0) {
+          $navbar.addClass("sticky");
+        } else {
+          $navbar.removeClass("sticky");  
+        }
+    });
+    
+    // //section sticky
+    // $(document).scroll(function() {
+    //     var scrollTop = $(this).scrollTop();
+    //     if ($(window).height() > scrollTop) {
+    //       $navbar.removeClass("sticky");
+    //     } else {
+    //       $navbar.addClass("sticky");  
+    //     }
+    // });
+
+})(jQuery, undefined);
+
+// $(".menu").click(function(){
+//   $("#nav").toggleClass("open");
+// });
+});
+// end of astronomy field page
+
+// start of tilt scroll:
 /* ===========================================================
  * jquery-tiltedpage-scroll.js v1.2.1
  * ===========================================================
@@ -115,41 +152,3 @@
   
 // }(window.jQuery);
 // end of tilt scroll
-
-// start of astronomy field page:
-document.addEventListener("turbolinks:load", function(){
-
-(function($) {
-    "use strict";
-  
-    var $navbar = $(".nav"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
-
-    //scroll top 0 sticky
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 0) {
-          $navbar.addClass("sticky");
-        } else {
-          $navbar.removeClass("sticky");  
-        }
-    });
-    
-    //section sticky
-    /*$(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if ($(window).height() > scrollTop) {
-          $navbar.removeClass("sticky");
-        } else {
-          $navbar.addClass("sticky");  
-        }
-    });*/
-
-})(jQuery, undefined);
-
-$(".menu").click(function(){
-  $("#nav").toggleClass("open");
-});
-});
-// end of astronomy field page
